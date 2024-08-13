e5f99c016a23329b47c24192d3a6a03da99c494d
GOB <goblin52@gmail.com>
2024-08-13 13:13:02 +0900

    Add headers and some tweaks

---
+++ src/M5UnitMETER.hpp
@@ -0,0 +1,32 @@
+/*
+ * SPDX-FileCopyrightText: 2024 M5Stack Technology CO LTD
+ *
+ * SPDX-License-Identifier: MIT
+ */
+/*!
+  @file M5UnitMETER.hpp
+  @brief Main header of M5UnitMETER
+
+  @mainpage M5UnitMETER
+  Library for UnitMETER using M5UnitUnified.
+*/
+#ifndef M5_UNIT_METER_HPP
+#define M5_UNIT_METER_HPP
+
+#include "unit/unit_Ameter.hpp"
+#include "unit/unit_Vmeter.hpp"
+
+/*!
+  @namespace m5
+  @brief Top level namespace of M5stack
+ */
+namespace m5 {
+
+/*!
+  @namespace unit
+  @brief Unit-related namespace
+ */
+namespace unit {}  // namespace unit
+}  // namespace m5
+
+#endif
