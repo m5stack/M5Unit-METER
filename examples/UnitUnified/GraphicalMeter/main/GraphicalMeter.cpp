5aca7273776ee875ad5a2b8be7c7d54bc27cc8b6
GOB <goblin52@gmail.com>
2025-03-24 17:26:06 +0900

    Cosmetic change

--- examples/UnitUnified/GraphicalMeter/main/GraphicalMeter.cpp
+++ examples/UnitUnified/GraphicalMeter/main/GraphicalMeter.cpp
@@ -184,6 +184,7 @@ void setup()
     M5_LOGI("getPin: SDA:%u SCL:%u", pin_num_sda, pin_num_scl);
 
     auto cfg = unit.config();
+
 #if defined(USING_UNIT_VMETER) || defined(USING_UNIT_AMETER)
     cfg.rate = m5::unit::ads111x::Sampling::Rate250;
 #elif defined(USING_UNIT_KMETER_ISO) || defined(USING_UNIT_DUAL_KMETER)
@@ -191,6 +192,7 @@ void setup()
 #else
 #error "Choose unit"
 #endif
+
     unit.config(cfg);
 
     auto ccfg        = unit.component_config();
