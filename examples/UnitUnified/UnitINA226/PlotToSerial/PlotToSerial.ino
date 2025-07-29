f522839eb29f5c8e41ef49bb7927bb9e348dea28
GOB <goblin52@gmail.com>
2025-07-29 19:18:40 +0900

    Add define for using unit

--- examples/UnitUnified/UnitINA226/PlotToSerial/PlotToSerial.ino
+++ examples/UnitUnified/UnitINA226/PlotToSerial/PlotToSerial.ino
@@ -6,4 +6,11 @@
 /*
   Example using M5UnitUnified for UnitINA226
 */
+// *************************************************************
+// Choose one define symbol to match the unit you are using
+// *************************************************************
+#if !defined(USING_UNIT_INA226_1A) && !defined(USING_UNIT_INA226_10A)
+// #define USING_UNIT_INA226_1A
+// #define USING_UNIT_INA226_10A
+#endif
 #include "main/PlotToSerial.cpp"
